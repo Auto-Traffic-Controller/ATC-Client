0a71f4e77523321b90c6da2c35671708347c781d
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-28 14:08:33 +0900

    Add font

--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -22,4 +22,14 @@ export const LogoText = styled.p`
   font-weight: 400;
   font-size: 1.625rem;
   margin-left: 0.5rem;
+
+  @font-face {
+    font-family: "GmarketSansMedium";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  font-family: "GmarketSansMedium";
 `;
