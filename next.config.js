ca64858ec1549ac467a41336c5c2ac5e2f98ba92
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-28 19:36:44 +0900

    Migrate to pnpm

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    emotion: true,
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
