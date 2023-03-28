2a8414ef8e07cb5c879b0a32dffb3b271bcafaaf
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-28 12:18:44 +0900

    Add Logo

---
+++ src/assets/Logo.tsx
@@ -0,0 +1,80 @@
+const Logo = () => (
+  <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="28" height="28" rx="8" fill="#49935C" />
+    <rect
+      x="2.79999"
+      y="2.80005"
+      width="22.4"
+      height="22.4"
+      rx="8"
+      fill="url(#paint0_linear_156_8131)"
+    />
+    <rect
+      x="5.59998"
+      y="5.59998"
+      width="16.8"
+      height="16.8"
+      rx="8"
+      fill="#49935C"
+    />
+    <rect
+      x="8.40002"
+      y="8.40002"
+      width="11.2"
+      height="11.2"
+      rx="5.6"
+      fill="url(#paint1_linear_156_8131)"
+    />
+    <rect x="11.2" y="11.2" width="5.6" height="5.6" rx="2.8" fill="#49935C" />
+    <rect
+      width="1.86667"
+      height="1.86667"
+      rx="0.933333"
+      transform="matrix(1 0 0 -1 13.0667 14.9333)"
+      fill="url(#paint2_linear_156_8131)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_156_8131"
+        x1="14"
+        y1="2.80005"
+        x2="14"
+        y2="25.2"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#CCE6D2" />
+        <stop offset="1" stop-color="#CCE6D2" stop-opacity="0" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_156_8131"
+        x1="14"
+        y1="8.40002"
+        x2="14"
+        y2="19.6"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#CCE6D2" />
+        <stop offset="1" stop-color="#CCE6D2" stop-opacity="0" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear_156_8131"
+        x1="0.933333"
+        y1="0"
+        x2="0.933333"
+        y2="1.86667"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#CCE6D2" />
+        <stop offset="1" stop-color="#CCE6D2" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default Logo;
