3978631ceb83b1ec66890cf5e426a044249a4c69
hyeongrok7874 <khr5k0228@gmail.com>
2023-03-28 20:01:03 +0900

    Add LoginIcon

---
+++ src/assets/LoginIcon.tsx
@@ -0,0 +1,22 @@
+const LoginIcon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_149_8048)">
+      <rect width="32" height="32" rx="16" fill="#BFC5C0" />
+      <ellipse cx="16" cy="28.5" rx="12" ry="7.5" fill="#A4ADA6" />
+      <circle cx="16" cy="13" r="6" fill="#A4ADA6" />
+    </g>
+    <defs>
+      <clipPath id="clip0_149_8048">
+        <rect width="32" height="32" rx="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default LoginIcon;
