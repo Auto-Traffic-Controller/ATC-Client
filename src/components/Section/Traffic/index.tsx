727abba63baea2de1772bd49e52964d4f49d6a2d
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 04:22:04 +0900

    Add TrafficSection frame

---
+++ src/components/Section/Traffic/index.tsx
@@ -0,0 +1,12 @@
+import { SectionTitle } from "../style";
+import * as S from "./style";
+
+const Traffic = () => {
+  return (
+    <S.TrafficSection>
+      <SectionTitle>네트워크 사용량</SectionTitle>
+    </S.TrafficSection>
+  );
+};
+
+export default Traffic;
---
+++ src/components/Section/Traffic/style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+import { Section } from "components/Section/style";
+
+export const TrafficSection = styled(Section)`
+  width: 74.7%;
+  background: #ffffff;
+  border-radius: 0.5rem;
+`;
