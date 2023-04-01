6822853deae879cc76af23c5077a08eb8b0c4114
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 04:19:03 +0900

    Add statusIcons

---
+++ src/assets/StatusIcons/index.ts
@@ -0,0 +1,3 @@
+export { default as Good } from "./Good";
+export { default as Normal } from "./Normal";
+export { default as Bad } from "./Bad";
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,6 @@
 export { default as Logo } from "./Logo";
 export { default as LoginIcon } from "./LoginIcon";
 export { default as Alarm } from "./alarm";
+export { Good as GoodStatus } from "./StatusIcons";
+export { Bad as BadStatus } from "./StatusIcons";
+export { Normal as NormalStatus } from "./StatusIcons";
