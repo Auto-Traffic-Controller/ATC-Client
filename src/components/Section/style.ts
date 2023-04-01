4da9ea54b3c6a8245fe727698f75361a769375fa
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 04:20:04 +0900

    Add Section shared style

---
+++ src/components/Section/style.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Section = styled.div`
+  background: #ffffff;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+`;
+
+export const SectionTitle = styled.p`
+  color: #000000;
+  font-size: 1rem;
+  font-weight: 700;
+`;
