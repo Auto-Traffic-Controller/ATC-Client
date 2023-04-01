7b82c412df83fc56457952b98f75021065a5e279
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 04:21:43 +0900

    Add BlockIPSection frame

---
+++ src/components/Section/BlockIP/index.tsx
@@ -0,0 +1,12 @@
+import { SectionTitle } from "components/Section/style";
+import * as S from "./style";
+
+const BlockIP: React.FC = () => {
+  return (
+    <S.BlockIPSection>
+      <SectionTitle>차단한 IP</SectionTitle>
+    </S.BlockIPSection>
+  );
+};
+
+export default BlockIP;
---
+++ src/components/Section/BlockIP/style.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import { Section } from "components/Section/style";
+
+export const BlockIPSection = styled(Section)`
+  width: 49.5%;
+`;
