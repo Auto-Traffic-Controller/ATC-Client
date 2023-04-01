fba1db0937f0c16445196c4edeb24d969b25995f
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 04:23:45 +0900

    Update Home style

--- src/styles/HomeStyle.ts
+++ src/styles/HomeStyle.ts
@@ -1,7 +1,14 @@
 import styled from "@emotion/styled";
 
 export const HomeContent = styled.div`
-  width: 79%;
-  // for test
+  width: 91.6%;
   margin: 3.5rem auto 0;
+`;
+
+export const SectionWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  :nth-of-type(2) {
+    margin-top: 1rem;
+  }
 `;
