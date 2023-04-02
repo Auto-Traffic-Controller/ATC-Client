ee075192659b83f2baa0c4fe6ee69f3208bcf5fd
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-03 01:09:40 +0900

    Add Section to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
-import { Header } from "components";
+import {
+  AttackLogSection,
+  BlockIPSection,
+  Header,
+  TrafficSection,
+  UsageSection,
+} from "components";
+import { HomeContent, SectionWrap } from "styles/HomeStyle";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <HomeContent>
+        <SectionWrap>
+          <TrafficSection />
+          <UsageSection />
+        </SectionWrap>
+        <SectionWrap>
+          <AttackLogSection />
+          <BlockIPSection />
+        </SectionWrap>
+      </HomeContent>
     </>
   );
 }
