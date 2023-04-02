e4dd49654e85899977f8b9af41340e9a02365cef
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-03 01:09:23 +0900

    Add export

---
+++ src/components/Section/index.ts
@@ -0,0 +1,4 @@
+export { default as Usage } from "./Usage";
+export { default as AttackLog } from "./AttackLog";
+export { default as Traffic } from "./Traffic";
+export { default as BlockIP } from "./BlockIP";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Header } from "./common/Header";
+export { Usage as UsageSection } from "./Section";
+export { AttackLog as AttackLogSection } from "./Section";
+export { Traffic as TrafficSection } from "./Section";
+export { BlockIP as BlockIPSection } from "./Section";
