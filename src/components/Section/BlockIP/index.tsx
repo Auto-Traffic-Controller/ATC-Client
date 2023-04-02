39f2cba7a2c7b604a52703d0a8430d06f008c9e6
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-02 14:23:38 +0900

    Add Section Header

--- src/components/Section/BlockIP/index.tsx
+++ src/components/Section/BlockIP/index.tsx
@@ -1,10 +1,42 @@
+import { css } from "@emotion/react";
 import { SectionTitle } from "components/Section/style";
+import { useState } from "react";
 import * as S from "./style";
 
+type NewOrOld = "new" | "old";
+
 const BlockIP: React.FC = () => {
+  const [newOrOld, setNewOrOld] = useState<NewOrOld>("new");
+
+  const selectStyle = (selectedItem: NewOrOld) =>
+    newOrOld === selectedItem &&
+    css`
+      background: #cce6d2;
+      color: #49935c;
+    `;
+
   return (
     <S.BlockIPSection>
-      <SectionTitle>차단한 IP</SectionTitle>
+      <S.SectionHeader>
+        <SectionTitle>차단한 IP</SectionTitle>
+        <S.FilterWrap>
+          <S.NewOrOldButton
+            onClick={() => setNewOrOld("new")}
+            css={selectStyle("new")}
+          >
+            최신
+          </S.NewOrOldButton>
+          <S.NewOrOldButton
+            onClick={() => setNewOrOld("old")}
+            css={selectStyle("old")}
+          >
+            오래된
+          </S.NewOrOldButton>
+          <S.AttackTypeFilter>
+            <option>공격유형</option>
+          </S.AttackTypeFilter>
+        </S.FilterWrap>
+      </S.SectionHeader>
     </S.BlockIPSection>
   );
 };
--- src/components/Section/BlockIP/style.ts
+++ src/components/Section/BlockIP/style.ts
@@ -4,3 +4,70 @@ import { Section } from "components/Section/style";
 export const BlockIPSection = styled(Section)`
   width: 49.5%;
 `;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+`;
+
+export const FilterWrap = styled.div``;
+
+export const NewOrOldButton = styled.button`
+  background: #f7f7f8;
+  border-radius: 0.25rem;
+  padding: 0.469rem 0.75rem;
+  margin-right: 0.5rem;
+  color: #a4ada6;
+  font-weight: 600;
+  font-size: 0.875rem;
+`;
+
+export const AttackTypeFilter = styled.select`
+  background: #cce6d2;
+  border-radius: 0.25rem;
+  color: #49935c;
+  font-weight: 600;
+  font-size: 0.875rem;
+  height: 2rem;
+  padding: 0.469rem 0.75rem;
+  border: none;
+`;
+
+export const ListHeader = styled.div`
+  background: #f7f7f8;
+  border-radius: 0.25rem;
+  height: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 0.5rem;
+`;
+
+export const ListHeaderText = styled.p`
+  width: 28%;
+  color: #616b64;
+  font-weight: 500;
+  font-size: 0.875rem;
+`;
+
+export const ListWrapper = styled.ul`
+  height: 83%;
+  overflow-y: scroll;
+`;
+
+export const ListElement = styled.li`
+  height: 2.5rem;
+  padding: 0.75rem 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.25rem 0;
+`;
+
+export const ListElementText = styled.p`
+  color: #303632;
+  font-weight: 600;
+  font-size: 0.75rem;
+  width: 28%;
+`;
