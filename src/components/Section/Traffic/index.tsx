b6746fc0e8d1767771cae14395f961422c269348
hyeongrok7874 <khr5k0228@gmail.com>
2023-04-03 01:41:41 +0900

    Add Traffic Category

--- src/components/Section/Traffic/index.tsx
+++ src/components/Section/Traffic/index.tsx
@@ -1,10 +1,31 @@
+import { css } from "@emotion/react";
 import { SectionTitle } from "../style";
 import * as S from "./style";
 
 const Traffic = () => {
   return (
     <S.TrafficSection>
-      <SectionTitle>네트워크 사용량</SectionTitle>
+      <S.SectionHeader>
+        <SectionTitle>네트워크 사용량</SectionTitle>
+        <S.TrafficCategoryWrap>
+          <S.TrafficCategory>
+            <S.TrafficValueCategory>Current</S.TrafficValueCategory>
+            <S.TrafficValue>1.20M</S.TrafficValue>
+          </S.TrafficCategory>
+          <S.TrafficCategory>
+            <S.TrafficValueCategory>Average</S.TrafficValueCategory>
+            <S.TrafficValue>7.53M</S.TrafficValue>
+          </S.TrafficCategory>
+          <S.TrafficCategory
+            css={css`
+              margin-right: 0;
+            `}
+          >
+            <S.TrafficValueCategory>Maximum</S.TrafficValueCategory>
+            <S.TrafficValue>30.94M</S.TrafficValue>
+          </S.TrafficCategory>
+        </S.TrafficCategoryWrap>
+      </S.SectionHeader>
     </S.TrafficSection>
   );
 };
--- src/components/Section/Traffic/style.ts
+++ src/components/Section/Traffic/style.ts
@@ -6,3 +6,28 @@ export const TrafficSection = styled(Section)`
   background: #ffffff;
   border-radius: 0.5rem;
 `;
+
+export const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const TrafficCategoryWrap = styled.div`
+  font-weight: 700;
+  font-size: 0.875rem;
+  display: flex;
+`;
+
+export const TrafficCategory = styled.div`
+  margin-right: 2rem;
+  display: flex;
+`;
+
+export const TrafficValueCategory = styled.p`
+  color: #a4ada6;
+  margin-right: 0.25rem;
+`;
+
+export const TrafficValue = styled.p`
+  color: #000000;
+`;
