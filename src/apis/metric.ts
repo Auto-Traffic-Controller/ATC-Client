1aa1fa42eac7d47369d6337cbbfc421a493d5c0f
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-08 19:44:06 +0900

    Update metric api

--- src/apis/metric.ts
+++ src/apis/metric.ts
@@ -1,22 +1,13 @@
-import { axiosClient } from "libs/api/axios/client";
+import { getRequest } from "libs/api/axios/client";
 import { useQuery } from "@tanstack/react-query";
 import { metricKey } from "./queryKeys";
 import { CPUMetricType, getNetworkMetricType } from "types/metric";
 import { metricUrl } from "libs/api/apiUrlController";
 
-async function getUsage(url: string) {
-  try {
-    const { data } = await axiosClient.get(url);
-    return data;
-  } catch (e) {
-    return e;
-  }
-}
-
 export function useGetCPUUsage() {
   const query = useQuery<CPUMetricType>(
     metricKey.CPU,
-    () => getUsage(metricUrl.getCPUUsage()),
+    () => getRequest(metricUrl.getCPUUsage()),
     {
       refetchInterval: 1000,
     }
@@ -30,7 +21,7 @@ export function useGetCPUUsage() {
 export function useGetNetworkPackets() {
   const query = useQuery<getNetworkMetricType>(
     metricKey.network,
-    () => getUsage(metricUrl.getNetworkPackets()),
+    () => getRequest(metricUrl.getNetworkPackets()),
     {
       refetchInterval: 1000,
     }
