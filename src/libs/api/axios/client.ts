68e469fc10c17fccb2ac07a53b0881ffd6eee455
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-08 19:43:06 +0900

    Add getRequest

--- src/libs/api/axios/client.ts
+++ src/libs/api/axios/client.ts
@@ -3,3 +3,12 @@ import axios from "axios";
 export const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
+
+export async function getRequest(url: string) {
+  try {
+    const { data } = await axiosClient.get(url);
+    return data;
+  } catch (e) {
+    return e;
+  }
+}
