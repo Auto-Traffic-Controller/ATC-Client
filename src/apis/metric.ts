4ddba9737efd49908b839036e27ded88408726c5
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-08 09:05:49 +0900

    Add metric api

---
+++ src/apis/metric.ts
@@ -0,0 +1,42 @@
+import { axiosClient } from "libs/api/axios/client";
+import { useQuery } from "@tanstack/react-query";
+import { metricKey } from "./queryKeys";
+import { CPUMetricType } from "types/metric";
+import { metricUrl } from "libs/api/apiUrlController";
+
+async function getUsage(url: string) {
+  try {
+    const { data } = await axiosClient.get(url);
+    return data;
+  } catch (e) {
+    return e;
+  }
+}
+
+export function useGetCPUUsage() {
+  const query = useQuery<CPUMetricType>(
+    metricKey.CPU,
+    () => getUsage(metricUrl.getCPUUsage()),
+    {
+      // refetchInterval: 1000,
+    }
+  );
+
+  const { data } = query;
+
+  return { cpuUsage: data?.cpu ?? 0 };
+}
+
+export function useGetNetworkPackets() {
+  const query = useQuery(
+    metricKey.network,
+    () => getUsage(metricUrl.getNetworkPackets()),
+    {
+      // refetchInterval: 1000,
+    }
+  );
+
+  const { data } = query;
+
+  return { networkPackets: data?.network ?? 0 };
+}
