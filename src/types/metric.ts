7e4a7476d17a072a8f10a1528d53006342c2f702
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-08 14:18:30 +0900

    Add metric type

--- src/types/metric.ts
+++ src/types/metric.ts
@@ -1,3 +1,12 @@
 export interface CPUMetricType {
   cpu: number;
 }
+
+interface NetworkMetricType {
+  timestamp: string;
+  packet: number;
+}
+
+export interface getAllNetworkMetricType {
+  network: NetworkMetricType[];
+}
