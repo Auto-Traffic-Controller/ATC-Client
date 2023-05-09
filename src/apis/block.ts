e7edf4a61ae7fb833be9d288509714bb3e5597a5
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-09 10:49:50 +0900

    Update block api

--- src/apis/block.ts
+++ src/apis/block.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { metricKey } from "./queryKeys";
+import { blockKey } from "./queryKeys";
 import { getRequest } from "libs/api/axios/client";
 import { blockUrl } from "libs/api/apiUrlController";
 import { getBlockIPType } from "types/block";
 
 export function useGetBlockIP() {
-  const query = useQuery<getBlockIPType>(metricKey.blockIP, () =>
+  const query = useQuery<getBlockIPType>(blockKey.blockIP, () =>
     getRequest(blockUrl.getBlockIP())
   );
 
