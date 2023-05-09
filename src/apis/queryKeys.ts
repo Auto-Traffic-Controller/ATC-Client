33273a411bafe4a51c6653a429588e568e553673
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-09 10:49:35 +0900

    Update queryKeys

--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -2,4 +2,13 @@ export const metricKey = {
   CPU: ["cpu"],
   network: ["network"],
   blockIP: ["blockIP"],
+  attackLog: ["attackLog"],
+};
+
+export const blockKey = {
+  blockIP: ["blockIP"],
+};
+
+export const attackKey = {
+  attackLog: ["attackLog"],
 };
